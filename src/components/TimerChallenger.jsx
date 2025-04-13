d8ba46b26ac06a1ff453fc0ba7bd4077ef371323
HarishLoghu <harishloghu@gmail.com>
2025-04-13 17:42:13 +0530

    feat:updated

--- src/components/TimerChallenger.jsx
+++ src/components/TimerChallenger.jsx
@@ -1,16 +1,48 @@
+import { useRef, useState } from "react";
+import ResultModal from "./ResultModel";
+
+// let timer;
+
 export default function TimerChallenger({title,targetTime}){
-  return <section className="challenge">
+
+  const timer = useRef();
+  const dialog = useRef();
+
+  const[timerStarted,setTimerStared] = useState(false);
+  const[timerExpired,setTimerExpired] = useState(false);
+  
+  function handleStart(){
+    timer.current =  setTimeout(()=>{
+      setTimerExpired(true);
+      dialog.current.showModal();
+    },target * 1000);
+
+    setTimerStared(true);
+  }
+
+  function handleStop(){
+      clearTimeout(timer.current);
+  }
+
+  return (
+  <>
+   <ResultModal ref={dialog} targetTime={targetTime} result="lost"/>
+    <section className="challenge">
     <h2>{title}</h2>
+    
     <p className="challenge-time">
       {targetTime} second{targetTime > 1 ? 's':''}
     </p>
     <p>
-      <button>
-        Stop Challenge
+      <button onClick={timerStarted ? handleStop:handleStart}>
+        {timerStarted ? 'Stop':'Start'} Challenge
       </button>
     </p>
-    <p className="">
-      Time is running...
+    <p className={timerStarted ? 'active':undefined}>
+      {timerStarted ? 'Time is running..' : 'Time inactive'}
     </p>
   </section>
+  </>
+
+  );
 }
